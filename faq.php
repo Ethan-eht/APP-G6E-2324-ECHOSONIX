0d4c29215ef598ab46fdfa0ca48bd1702aa47053
Ethan-eht <105300067+Ethan-eht@users.noreply.github.com>
2024-01-24 19:01:08 +0100

    Last change FAQ

--- faq.php
+++ faq.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>FAQ Page</title>
-    <link rel="stylesheet" href="karl.css">
+    <link rel="stylesheet" href="stylefaq.css">
 
     <?php include 'header.php'; ?>
 
