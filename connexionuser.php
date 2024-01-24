79f2cecff733b44ec519f300ef1f05a9cdf43c1e
Ethan-eht <105300067+Ethan-eht@users.noreply.github.com>
2024-01-24 01:53:14 +0100

    Update connexionuser.php

--- connexionuser.php
+++ connexionuser.php
@@ -1,79 +1,38 @@
 <?php
+session_start();
 
 $servername = "localhost";
-
 $username = "root";
-
 $password = "";
 
- 
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=mydb", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    echo "Erreur : ". $e->getMessage();
+}
 
-    try {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-        $bdd = new PDO("mysql:host=$servername;dbname=inscription", $username, $password);
+    if ($email !=""&& $password != "") {
+        // Utilisez des requêtes préparées pour éviter les injections SQL
+        $requete = $bdd->prepare("SELECT * FROM utilisateurs WHERE mail = :email AND mdp = :password");
+        $requete->execute(['email' => $email, 'password' => $password]);
 
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $reponse = $requete->fetch();
 
-    }
-
-    catch(PDOException $e){
-
-        echo "Erreur : ". $e->getMessage();
-
-    }
-
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $email = $_POST["email"];
-
-        $password = $_POST["password"];
-
-      
-
-        if ($email !=""&& $password != "") {
-
-            $requete = $bdd->query("SELECT * FROM user WHERE email = '$email' AND mdp = '$password'");
-
-            $reponse = $requete->fetch();
-
-          
-
-            if($reponse["id"] != false) {
-
-                 echo " VOus ètes connecté !";
-
-                 header("Location:#");
-
-                 exit();
-
- 
-
-            }
-
-                else {
-
-                    $error_msg = "Email ou mdp incorrect !";
-
- 
-
+        if($reponse["idutilisateurs"] != false) {
+            $_SESSION['user_id'] = $reponse["idutilisateurs"]; // Set session variable
+            echo " VOus ètes connecté !";
+            header("Location:accueil.php");
+            exit();
         }
-
+        else {
+            echo "Email ou mdp incorrect !";
+        }
     }
-
 }
-
-?>
-
-<?php
-
-if ($error_msg){
-
-    ?>
-
-    <p> <?php echo $error_msg; ?></p>
-
-    <?php
-
-}
-
 ?>
