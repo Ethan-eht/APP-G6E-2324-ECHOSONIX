5c051227416627da2c8986193e15a8bcccb8e701
Lucasrkt <151949991+Lucasrkt@users.noreply.github.com>
2024-01-24 16:18:44 +0100

    amelioration inscription.php

--- inscription.php
+++ inscription.php
@@ -34,60 +34,41 @@
 <body>
 
 
-    <form method="POST" action="traitementlucas.php">
-
+    <form method="POST" action="traitementlucas.php"  id="form">
         <h4>INSCRIPTION</h4>
-
         <hr>
-
         <div class="nomprenom">
-
-
-
-            <label for="nom">Votre Nom </label>
-
-            <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..." >
-
-            <br>
-
-            <label class="nom" for="prenom">Votre Prenom </label>
-
-            <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prenom..." >
-
-
-
+            
+                <label for="nom">Votre Nom </label>
+                <input type="text" id="nom" name="nom" placeholder="Entrez votre nom..." >
+                <span class="error" id="nomError"></span>
+            
+                <label class="nom" for="prenom">Votre Prenom </label>
+                <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prenom..."  >
+                <span class="error" id="prenomError"></span>
+            
         </div>
-
         <br>
-
         <label for="pseudo">Votre Pseudo </label>
-
         <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo..." class="txt" >
-
+        <span class="error" id="pseudoError"></span>
         <br>
-
         <label for="email">Votre Email</label>
-
         <input type="email" id="email" name="email" placeholder="Entrez votre email..." class="txt" >
-
+        <span class="error" id="emailError"></span>
         <br>
-
         <label for="mdp">Votre Mot de passe</label>
-
-        <input type="password" id="mdp" name="mdp" placeholder="Entrez votre mdp..." class="txt" >
-
+        <input type="password" id="mdp" name="mdp" placeholder="Entrez votre mdp..." class="txt">
+        <span class="error" id="mdpError"></span>
         <br>
-
-        <button type="submit" value="M'inscrire" name="ok" class="btn"> M'inscrire </button>
-
+        <button type="submit" value="M'inscrire" name="ok" class="btn" > M'inscrire </button>
         <br>
-
-        <p>Vous avez déjà un compte ? <a href="connexion.php">Se connecte </a></p>
-
+        <p>Vous avez déjà un compte ? <a href="login.php">Se connecte </a></p>
     </form>
-
+     
+     
 
 </body>
 <script src="index.js"></script>
-
 </html>
+
